63062355fe306b4e6d3ac22080df3d7a45aa611d
SethGuimont <ur4634zc@metrostate.edu>
2021-09-14 19:47:20 -0500

    initial

---
+++ starterbot.py
@@ -0,0 +1,24 @@
+import os
+import sys
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
+
+import os
+from slack_sdk import WebClient
+from slack_sdk.errors import SlackApiError
+
+slack_token = os.environ["SLACK_BOT_TOKEN"]
+client = WebClient(token=slack_token)
+script = (os.popen("sh test.sh").read())
+
+try:
+    response = client.chat_postMessage(
+        channel="testing",
+        text=script
+
+    )
+except SlackApiError as e:
+    # You will get a SlackApiError if "ok" is False
+    assert e.response["error"]
+
